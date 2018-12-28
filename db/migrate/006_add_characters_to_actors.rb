baa1a021d17a945cd70db3f27e0ddbcc3c553cf4
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-28 20:33:03 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_characters_to_actors.rb
+++ db/migrate/006_add_characters_to_actors.rb
@@ -1,4 +1,4 @@
-class AddCharactersToActor < ActiveRecord::Migration[4.2]
+class AddCharactersToActors < ActiveRecord::Migration[4.2]
   
   def change
     add_column :characters, :actor_id, :integer
