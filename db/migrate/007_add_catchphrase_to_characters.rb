f881672efb1bbb6d2c3eafc3ea4088d1ebcb049f
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-28 20:39:40 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_catchphrase_to_characters.rb
+++ db/migrate/007_add_catchphrase_to_characters.rb
@@ -1,6 +1,6 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[4.2]
   
   def change
-    add_column :characters, :catchprase, :string
+    add_column :characters, :catchphrase, :string
   end
 end
