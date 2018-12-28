e06b09345f93f42b54c338c22e5effd088c3fc6f
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-28 20:30:41 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_characters_to_actors.rb
+++ db/migrate/006_add_characters_to_actors.rb
@@ -1,8 +1,6 @@
-class AddShowsToNetwork < ActiveRecord::Migration[4.2]
-  #to make an additive change to a schema we create
-  #a new migration, and then in the change method,
-  #we make the change.
+class AddCharactersToActor < ActiveRecord::Migration[4.2]
+  
   def change
-    add_column :shows, :network_id, :integer
+    add_column :characters, :actor_id, :integer
   end
 end
